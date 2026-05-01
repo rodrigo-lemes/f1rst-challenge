260ceb17dda8417f091834a2a5f4e41ec44cb6cc
Rodrigo Soukef Lemes dos Santos <rodrigo.lemes@gmail.com>
2026-05-01 17:24:21 -0300

    front-end ok

--- address-finder-front/src/app/app.ts
+++ address-finder-front/src/app/app.ts
@@ -1,11 +1,71 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AddressService } from './address.service';
+import { AddressResponse } from './address-response';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('address-finder-front');
+
+  zipCode = signal('');
+  address = signal<AddressResponse | undefined>(undefined);
+  loading = signal(false);
+  errorMessage = signal('');
+
+  constructor(private readonly addressService: AddressService) {
+  }
+
+  searchAddress(): void {
+    this.errorMessage.set('');
+    this.address.set(undefined);
+
+    const normalizedZipCode = this.zipCode().trim();
+
+    if (!normalizedZipCode) {
+      this.errorMessage.set('Informe um CEP para consulta.');
+      return;
+    }
+
+    this.loading.set(true);
+
+    this.addressService.findByZipCode(normalizedZipCode).subscribe({
+      next: (response) => {
+        console.log('Address response:', response);
+
+        this.address.set(response);
+        this.loading.set(false);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error('Address search error:', error);
+
+        this.errorMessage.set(this.resolveErrorMessage(error));
+        this.loading.set(false);
+      }
+    });
+  }
+
+  private resolveErrorMessage(error: HttpErrorResponse): string {
+    if (error.status === 400) {
+      return 'CEP inválido. Use o formato 13458870 ou 13458-870.';
+    }
+
+    if (error.status === 404) {
+      return 'CEP não encontrado.';
+    }
+
+    if (error.status === 502) {
+      return 'Erro ao consultar a API externa de endereço.';
+    }
+
+    return 'Erro inesperado ao buscar o endereço.';
+  }
 }
